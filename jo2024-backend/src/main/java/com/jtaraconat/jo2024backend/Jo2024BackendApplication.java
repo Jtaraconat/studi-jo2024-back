5fad7341f35f73ab9e9a45a9f89c580aa00e88a0
Jtaraconat <johan.taraconat@gmail.com>
2024-05-10 16:04:41 +0200

    maven init

---
+++ jo2024-backend/src/main/java/com/jtaraconat/jo2024backend/Jo2024BackendApplication.java
@@ -0,0 +1,13 @@
+package com.jtaraconat.jo2024backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jo2024BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jo2024BackendApplication.class, args);
+	}
+
+}
